67c50a8466121bdc810ad30ffed2b3ac39ba6603
Ishmael Rogers <41645540+IshmaelRogers@users.noreply.github.com>
2018-11-26 13:11:44 -0500

    Create compute_normal_histograms.py

---
+++ RoboND/Helper Functions/compute_normal_histograms.py
@@ -0,0 +1,33 @@
+def compute_normal_histograms(normal_cloud):
+    norm_x_vals = []
+    norm_y_vals = []
+    norm_z_vals = []
+
+    for norm_component in pc2.read_points(normal_cloud,
+                                          field_names = ('normal_x', 'normal_y', 'normal_z'),
+                                          skip_nans=True):
+        norm_x_vals.append(norm_component[0])
+        norm_y_vals.append(norm_component[1])
+        norm_z_vals.append(norm_component[2])
+
+    # Compute histograms of normal values (just like with color)
+
+    nbins=32
+    bins_range=(-1, 1)
+        
+    # Compute the histogram of the channels separately
+    norm_x_hist = np.histogram(norm_x_vals, bins=nbins, range=bins_range)
+    norm_y_hist = np.histogram(norm_y_vals, bins=nbins, range=bins_range)
+    norm_z_hist = np.histogram(norm_z_vals, bins=nbins, range=bins_range)
+    
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((norm_x_hist[0], norm_y_hist[0], norm_z_hist[0])).astype(np.float64)
+    
+    # Normalize the result
+    normed_features = hist_features / np.sum(hist_features)
+
+    # Generate random features for demo mode.  
+    # Replace normed_features with your feature vector
+    #normed_features = np.random.random(96)
+
+    return normed_features
